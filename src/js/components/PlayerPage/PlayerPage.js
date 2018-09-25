097510512915131105faa6cf25ca1109451ee816
SlikSlaks <java_kavai@live.ru>
2018-09-25 23:29:39 +0300

    Добавил переключение треков Добавил отображение длительности треков

--- src/js/components/PlayerPage/PlayerPage.js
+++ src/js/components/PlayerPage/PlayerPage.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Player from "../Player/Player";
 import TrackList from "../TrackList/TrackList";
 import "./player-page.less"
 
@@ -14,8 +13,7 @@ export default class PlayerPage extends React.Component{
         const {track} = this.state;
         return (
             <div className={"player-page"}>
-                <TrackList onClick={(track)=>this.setState({track})}/>
-                <Player track={track}/>
+                <TrackList onClick={(track)=>this.setState({track})} track={track}/>
             </div>
         )
     }
--- src/js/components/TrackList/TrackList.js
+++ src/js/components/TrackList/TrackList.js
@@ -1,33 +1,59 @@
 import React from "react";
 import "./track-list.less"
+import {secondsToTime} from "../../utils/utils";
+import Player from "../Player/Player";
+import PropTypes from "prop-types"
 
 export default class TrackList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             tracks: [],
-            activeTrack: null
-        }
+            activeTrack: null,
+            gotDuration: false
+        };
+
+        this.trackRow = [];
+        this.setTrackRow = element => {
+            this.trackRow.push(element);
+        };
+        this.getTrackDuration = ( i ) => {
+            if (this.trackRow[ i ]) return this.trackRow[ i ].duration
+        };
+
     }
+
     componentDidMount() {
-        fetch("http://localhost:3000/tracks").then(res => res.json()).then(res => this.setState({ tracks: res }))
+        fetch("http://localhost:3000/tracks").then(res => res.json()).then(res => this.setState({tracks: res}))
     }
-    selectTrack(track) {
-        const { onClick } = this.props;
-        onClick("http://localhost:3000/tracks/" + track);
-        this.setState({ activeTrack: track })
+
+    selectTrack(index) {
+        if(this.state.activeTrack!=null) this.trackRow[this.state.activeTrack].pause();
+        this.trackRow[index].play();
+        this.setState({activeTrack: index})
     }
+
+    onCanPlay( ) {
+        this.setState({gotDuration: true});
+    }
+
     render() {
-        const { tracks,activeTrack } = this.state;
-        console.log(activeTrack)
+        const {tracks, activeTrack} = this.state;
         return (
             <div className={"track-list"}>
                 <ul>
-                    {tracks.map((track) => {
+                    {tracks.map((track, index) => {
                         return <li key={track}
-                                    className={track == activeTrack ? "active" : ""}
-                                    onClick={() => this.selectTrack(track)}>
+                                   className={track === tracks[activeTrack] ? "active" : ""}
+                                   onClick={() => this.selectTrack(index)}>
                             {track}
+                            {secondsToTime(this.getTrackDuration(index))}
+                            <audio
+                                ref={this.setTrackRow}
+                                preload={'auto'}
+                                onCanPlay={ this.onCanPlay.bind( this )}
+                                src={'http://localhost:3000/tracks/' + track}>
+                            </audio>
                         </li>
                     })}
                 </ul>
@@ -35,3 +61,7 @@ export default class TrackList extends React.Component {
         )
     }
 }
+
+Player.propTypes={
+    track:PropTypes.string
+};
