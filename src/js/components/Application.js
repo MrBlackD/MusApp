0faf7d71cffe48b9388df293eb813487276143f9
Denis <blackpirates@mail.ru>
2018-10-02 22:13:08 +0300

    Добавил prettier и commit hook для него

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -4,8 +4,20 @@ import Player from "./Player/Player";
 import TrackList from "./TrackList/TrackList";
 import PlayerPage from "./PlayerPage/PlayerPage";
 
-export default class Application extends React.Component{
-    render(){
-        return <PlayerPage/>
-    }
+export default class Application extends React.Component {
+  render() {
+    const test = [
+      "тестовый",
+      "коммит",
+      "для",
+      "проверки",
+      "коммит",
+      "хука",
+      "с",
+      "обработкой",
+      "через",
+      "prettier"
+    ];
+    return <PlayerPage />;
+  }
 }
