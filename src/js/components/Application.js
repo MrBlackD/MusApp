e43f04bff8146166c7e6618d029dfac2c3114ed4
Denis <blackpirates@mail.ru>
2018-10-02 22:14:02 +0300

    Подчистил тестовые данные

--- src/js/components/Application.js
+++ src/js/components/Application.js
@@ -6,18 +6,6 @@ import PlayerPage from "./PlayerPage/PlayerPage";
 
 export default class Application extends React.Component {
   render() {
-    const test = [
-      "тестовый",
-      "коммит",
-      "для",
-      "проверки",
-      "коммит",
-      "хука",
-      "с",
-      "обработкой",
-      "через",
-      "prettier"
-    ];
     return <PlayerPage />;
   }
 }
